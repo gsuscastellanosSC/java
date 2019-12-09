2968a1f09cbab1e331a203463db6db26c6a819e0
SC <jesuscastellanospaez@hotmail.com>
2019-12-09 14:01:37 -0500

    Primera Clase en java

---
+++ CursoJavaSEOrientadoAObjetos/src/EntenderLaProgramacionOrientadaAObjetos/Doctor.java
@@ -0,0 +1,14 @@
+package EntenderLaProgramacionOrientadaAObjetos;
+
+public class Doctor {
+	//Atributos
+	int id;
+	String name;
+	String speciality;
+	//Constructor
+	
+	//Metodoss
+	public void showName() {
+		System.out.println(name);
+	}
+}
---
+++ CursoJavaSEOrientadoAObjetos/src/EntenderLaProgramacionOrientadaAObjetos/Main.java
@@ -0,0 +1,14 @@
+package EntenderLaProgramacionOrientadaAObjetos;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Doctor d1 = new Doctor();
+		d1.id = 1;
+		d1.name = "Jesús";
+		d1.speciality = "Cardiologo";
+		d1.showName();
+	}
+
+}
