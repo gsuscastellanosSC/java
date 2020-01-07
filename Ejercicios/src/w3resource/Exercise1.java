a945264856ca189d22d7f149322dd03278a6e4bf
SC <jesuscastellanospaez@hotmail.com>
2020-01-07 11:07:35 -0500

    Solución del ejecicio II de colecciones

---
+++ Ejercicios/src/w3resource/Exercise1.java
@@ -0,0 +1,13 @@
+package w3resource;
+import java.util.*;
+public class Exercise1 {
+  public static void main(String[] args) {
+  List<String> list_Strings = new ArrayList<String>();
+  list_Strings.add("Red");
+  list_Strings.add("Green");
+  list_Strings.add("Orange");
+  list_Strings.add("White");
+  list_Strings.add("Black");
+  System.out.println(list_Strings);
+ }
+}
---
+++ Ejercicios/src/w3resource/Hello.java
@@ -0,0 +1,8 @@
+package w3resource;
+
+public class Hello {
+		  public static void main(String[] args) {
+		  // Write your code here
+		  System.out.println("hello\n"+"Jesus");
+		 }
+}
