1009c1449f1fbe1b84dda7f1d556689a95437fbb
SC <jesuscastellanospaez@hotmail.com>
2020-01-07 11:53:27 -0500

    Exercise7 saber si un elemento está dentor de un ArrayList

---
+++ Ejercicios/src/w3resource_Colecciones/Exercise7.java
@@ -0,0 +1,23 @@
+package w3resource_Colecciones;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Exercise7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<String> list_Strings = new ArrayList<String>();
+		list_Strings.add("Red");
+		list_Strings.add("Green");
+		list_Strings.add("Orange");
+		list_Strings.add("White");
+		list_Strings.add("Black");
+		System.out.println(searchAnElement(list_Strings, "Red"));
+		System.out.println("Despues "+ list_Strings);
+	}
+	public static boolean searchAnElement(List<String> liStrings, String elemtSearch){
+		liStrings.contains(elemtSearch);
+		return liStrings.contains(elemtSearch);
+	}
+}
